36434e1639f963d02a90dd246945e8cc892df7ba
kelostrada <kelostrada@gmail.com>
2012-05-14 22:01:05 +0000

    Dodanie przykładowych plików

---
+++ ASD2/Text/Simple.cs
@@ -0,0 +1,54 @@
+﻿
+namespace ASD.Text
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Klasa statyczna zawierająca implementację różnych metod wyszukiwania wzorców w tekście
+    /// </summary>
+    public static partial class StringMatching
+    {
+
+        /// <summary>
+        /// Wyszukiwanie wzorca w tekście algorytmem naiwnym
+        /// </summary>
+        /// <param name="y">Badany tekst</param>
+        /// <param name="x">Szukany wzorzec</param>
+        /// <returns>Lista zawierająca początkowe indeksy wystąpień wzorca x w tekście y</returns>
+        public static List<int> Simple(string y, string x)
+        {
+            int n, m, i, j;
+            n = y.Length;
+            m = x.Length;
+            List<int> ml = new List<int>();
+            for (i = 0; i <= n - m; ++i)
+            {
+                for (j = 0; j < m && y[i + j] == x[j]; ++j) ;
+                if (j == m) ml.Add(i);
+            }
+            return ml;
+        }
+
+        /// <summary>
+        /// Wyszukiwanie wzorca w tekście algorytmem "naiwnym wstecz"
+        /// </summary>
+        /// <param name="y">Badany tekst</param>
+        /// <param name="x">Szukany wzorzec</param>
+        /// <returns>Lista zawierająca początkowe indeksy wystąpień wzorca x w tekście y</returns>
+        public static List<int> SimpleBackward(string y, string x)
+        {
+            int n, m, i, j;
+            n = y.Length;
+            m = x.Length;
+            List<int> ml = new List<int>();
+            for (i = 0; i <= n - m; ++i)
+            {
+                for (j = m - 1; j >= 0 && y[i + j] == x[j]; --j) ;
+                if (j == -1) ml.Add(i);
+            }
+            return ml;
+        }
+
+    } // class StringMatching
+
+} // namespace ASD.Text
